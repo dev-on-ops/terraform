65dfba94efdbd6954f6fd5d7a31a680e01f044d8
Devon Berta <47953252+dev-on-ops@users.noreply.github.com>
2024-02-02 12:14:10 -0600

    Update main.tf

--- azure/network-module/main.tf
+++ azure/network-module/main.tf
@@ -7,7 +7,24 @@ provider "azurerm" {
 locals {
   network_configs = var.network_config
 }
-
+locals {
+  virtual_network_peerings = flatten([
+    for source_network_name, source_network_config in local.network_config : [
+      for target_network_name, target_network_config in local.network_config : 
+      # Exclude self-peering and existing peerings
+      if source_network_name != target_network_name &&
+         !contains(target_network_config.peered_with, source_network_name) : {
+        source_virtual_network_name    = source_network_config.virtual_network_name
+        source_resource_group_name     = source_network_config.resource_group_name
+        target_virtual_network_name    = target_network_config.virtual_network_name
+        target_resource_group_name     = target_network_config.resource_group_name
+        source_address_space           = source_network_config.address_space
+        target_address_space           = target_network_config.address_space
+        source_to_target_direction     = "bidirectional"
+      }
+    ]
+  ])
+}
 resource "azurerm_resource_group" "rg" {
   for_each = local.network_configs
 
